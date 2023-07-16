cc7f530739e670fcb54ddba1e4d1565d46523521
mmorimoto <maran.morimoto@mercadolivre.com>
2023-07-16 12:23:27 -0300

    creating entity with lombok e spring annotations

---
+++ src/main/java/com/example/finance/entities/User.java
@@ -0,0 +1,18 @@
+package com.example.finance.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity // utilizo para indicar ao JPA que essa classe é uma entidade.
+@Data // utilizo para gerar os métodos getters e setters, toString, equals e hashCode.
+public class User {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String name;
+  private String email;
+  private String password;
+}
