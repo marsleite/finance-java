94f78430a0cb9658e4128f516265e0472a0d3cb6
mmorimoto <maran.morimoto@mercadolivre.com>
2023-07-19 21:42:21 -0300

    adding DS_STORE to gitignore and fixing annotation for auto generation id

--- src/main/java/com/example/finance/entities/User.java
+++ src/main/java/com/example/finance/entities/User.java
@@ -2,6 +2,7 @@ package com.example.finance.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -10,7 +11,7 @@ import lombok.Data;
 public class User {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String name;
   private String email;
